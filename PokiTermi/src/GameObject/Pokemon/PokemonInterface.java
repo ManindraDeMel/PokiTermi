f80a4432da63c6e6b25dd0fe304e7c73f43c3d3a
Manindra De Mel <u7156805@anu.edu.au>
2023-09-20 20:11:09 +1000

    Delete PokemonInterface.java

--- PokiTermi/src/GameObject/Pokemon/PokemonInterface.java
+++
@@ -1,7 +0,0 @@
-package GameObject.Pokemon;
-
-public interface PokemonInterface {
-    public ActionResult attack(Pokemon pokemon);
-    public ActionResult defence(Pokemon pokemon);
-    public ActionResult cast(Pokemon pokemon);
-}
