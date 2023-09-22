28ffce651d50f924b12536a7a1abd0469e124acb
Manindra de Mel <manindrademel@yahoo.com.au>
2023-09-22 19:57:53 +1000

    Edit POM file and Build game

--- PokiTermi/src/Main.java
+++ PokiTermi/src/Main.java
@@ -4,18 +4,10 @@ public class Main {
 
     static MapTest mapTest = new MapTest();
     public static void main(String[] args)  {
-
-
         try{
             MapTest.runMapTest();
         }catch (Exception e){
             throw new RuntimeException(e);
         }
-
     }
-
-
-
-
-
 }
