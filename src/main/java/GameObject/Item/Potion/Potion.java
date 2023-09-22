457980b9ae78a1a71229c10c61640d465b162f48
Yusen Nian <u7572670@anu.edu.au>
2023-09-22 20:20:11 +0800

    Finish building Item

--- src/main/java/GameObject/Item/Potion/Potion.java
+++ src/main/java/GameObject/Item/Potion/Potion.java
@@ -3,21 +3,13 @@ package GameObject.Item.Potion;
 import GameObject.Item.Item;
 
 public class Potion extends Item {
-    private int healAmount;
+    public int healAmount;
+    public int num;
 
-    public Potion() {
+    public Potion(int num) {
         super();
         super.setName("Potion");
-        this.healAmount = 20; // Heals 20 HP
-    }
-
-    @Override
-    public void use() {
-        // Logic to heal a Pokémon goes here
-    }
-
-    @Override
-    public void display() {
-        // Logic to display the item's details goes here
+        this.healAmount = 50; // Heals 50 HP
+        this.num = num;
     }
 }
