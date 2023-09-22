3f8a724f11ec0b260c94e7d21f26ec1c80a6016c
Yusen Nian <u7572670@anu.edu.au>
2023-09-22 20:20:11 +0800

    Finish building Item

--- src/main/java/GameObject/Item/Potion/Potion.java
+++ src/main/java/GameObject/Item/Potion/Potion.java
@@ -1,43 +1,15 @@
-/**
- * Subclass of Item class, used to define potions.
- *
- * @author Yusen Nian
- */
 package GameObject.Item.Potion;
 
 import GameObject.Item.Item;
 
 public class Potion extends Item {
-    private int healAmount;
-    private int quantity;
+    public int healAmount;
+    public int num;
 
-    public Potion(int quantity) {
+    public Potion(int num) {
         super();
         super.setName("Potion");
         this.healAmount = 50; // Heals 50 HP
-        this.quantity = quantity;
-    }
-
-    public Potion(int quantity, int healAmount) {
-        super();
-        super.setName("Potion");
-        this.healAmount = healAmount;
-        this.quantity = quantity;
-    }
-
-    public int getHealAmount() {
-        return healAmount;
-    }
-
-    public void setHealAmount(int healAmount) {
-        this.healAmount = healAmount;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+        this.num = num;
     }
 }
