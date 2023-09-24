88212b29a31b35b02a045259e22593ef3c7afc0c
Zhangheng Xu <u6818456@anu.edu.au>
2023-09-24 23:37:47 +1000

    finished tooltips

--- src/main/java/Game/MapTest.java
+++ src/main/java/Game/MapTest.java
@@ -41,13 +41,34 @@ public class MapTest {
     public static Player player=new Player();
 
     // Define the position (x, y) and dimensions (width, height) of the title box
-    static int titleBoxX = 25;        // X-coordinate
+    static int titleBoxX = 20;        // X-coordinate
     static int titleBoxY = 0;        // Y-coordinate
-    static int titleBoxWidth = 25;   // Width of the text box
-    static int titleBoxHeight = 10;  // Height of the text box
+    static int titleBoxWidth = 30;   // Width of the title text box
+    static int titleBoxHeight = 3;  // Height of the title text box
+    static int toolTipBoxX = 50;
+    static int toolTipBoxY = 0;
+    static int toolTipWidth = 30;
+    static int toolTipHeight = 20;
 
-    // Create an instance of the TextBox class
+    // Create instance of the TextBox class
     static TextBox titleBox = new TextBox(titleBoxX, titleBoxY, titleBoxWidth, titleBoxHeight);
+    static TextBox toolTipBox = new TextBox(toolTipBoxX,toolTipBoxY,toolTipWidth,toolTipHeight);
+
+    // Set the text for the textBox
+    static String titleBoxText = "****Welcome to PokiTermi****";
+    static String toolTipBoxText = "Ohayo,Professor Oak. You got drunk last night and forgot how to move. Don't worry,  here are some tips to move  around.                     " +
+            "go north by click 'w'       " +
+            "go south by click 's'       " +
+            "go west by click 'a'        " +
+            "go east by click 'd'        " +
+            "----------------------------" +
+            "In PokiTermi world, you are " +
+            "pink '@', you can find white" +
+            " 'O' as rock,green 'F' as   tree, cyan '~' as   water.  " +
+            "u can talk with npc blue '@'" +
+            "fight with enemy red '@'    " +
+            "get item from chest yellow'?" +
+            "go to next level by white'D'";
 
 
 
@@ -89,12 +110,13 @@ public class MapTest {
             displayMap();
             showItemAround();
 
-            // Set the text for the textBox
-            String textBoxText = "PokiTermiiiiiiiiiiiiiiiiiiiaaaaaaaaiiiiiiiiiiiiiiiiiiiiii";
-            titleBox.setText(textBoxText);
 
+            // set the text box
+            titleBox.setText(titleBoxText);
+            toolTipBox.setText(toolTipBoxText);
             // Render the textBox
             titleBox.render(terminal);
+            toolTipBox.render(terminal);
 
             KeyStroke keyStroke = terminal.readInput();
             if(keyStroke != null){
