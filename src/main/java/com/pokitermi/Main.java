087025e7208a075ddd72c309918b44fbd32543c5
Yiming Lu <u7531243@anu.edu.au>
2023-09-26 23:19:59 +1000

    start restructuring the game

--- src/main/java/com/pokitermi/Main.java
+++ src/main/java/com/pokitermi/Main.java
@@ -4,10 +4,10 @@ import Game.MapTest;
 
 public class Main {
 
-    static MapTest mapTest = new MapTest();
     public static void main(String[] args)  {
         try{
             MapTest.runMapTest();
+
         }catch (Exception e){
             throw new RuntimeException(e);
         }
